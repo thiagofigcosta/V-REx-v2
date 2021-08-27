e07bb8678391dfda19660bc29377494fd9fa972c
Computa Hacker Boy <thiagofigcosta@hotmail.com>
2021-08-27 17:55:14 -0300

    tab to pretify logs

--- core-2/src/Core.py
+++ core-2/src/Core.py
@@ -331,10 +331,10 @@ class Core(object){
 			}
 			Core.LOGGER.info('Evaluated network...OK')
 			Core.LOGGER.info(train_metric.toKerasName())
-			Core.LOGGER.info(str(train_eval_res))
+			Core.LOGGER.info('\t'+str(train_eval_res))
 			if test_eval_res is not None{
 				Core.LOGGER.info(test_metric.toKerasName())
-				Core.LOGGER.info(str(test_eval_res))
+				Core.LOGGER.info('\t'+str(test_eval_res))
 			}
 			Core.LOGGER.info('Writing results...')
 			self.appendStatsOnNeuralNet(independent_net_id,'train_stats',train_eval_res,train_metric.toKerasName())
